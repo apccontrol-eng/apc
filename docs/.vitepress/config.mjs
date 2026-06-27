cf384cb060ed3dde2512d459753079422e9980b6
apccontrol-eng <286005338+apccontrol-eng@users.noreply.github.com>
2026-06-27 15:06:59 +0300

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,6 +1,12 @@
+import mathjax3 from 'markdown-it-mathjax3'
+
 export default {
   title: "Project Docs",
   base: "/apc/",
+  markdown: {
+    config: (md) => {
+      md.use(mathjax3)
+    }
   themeConfig: {
     sidebar: [
       {

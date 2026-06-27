a41cf64a5a85ce4259a6bb531266ac8c01350826
apccontrol-eng <286005338+apccontrol-eng@users.noreply.github.com>
2026-06-27 13:50:28 +0300

    docs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,5 +1,25 @@
 export default {
   title: "Project Docs",
-  description: "Documentation for my project",
-  base: "/apc/",
+  description: "Intro, MPC, and Process Monitoring",
+
+  themeConfig: {
+    nav: [
+      { text: "Intro", link: "/intro" },
+      { text: "MPC", link: "/mpc" },
+      { text: "Process Monitoring", link: "/process-monitoring" },
+    ],
+
+    sidebar: {
+      "/": [
+        {
+          text: "Documentation",
+          items: [
+            { text: "Intro", link: "/intro" },
+            { text: "MPC", link: "/mpc" },
+            { text: "Process Monitoring", link: "/process-monitoring" },
+          ],
+        },
+      ],
+    },
+  },
 }

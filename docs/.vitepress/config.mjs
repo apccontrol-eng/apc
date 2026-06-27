5211eeff18c82ba0a9d9f66fa0a23e9ac317aeac
apccontrol-eng <286005338+apccontrol-eng@users.noreply.github.com>
2026-06-27 14:51:28 +0300

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,32 +1,16 @@
-import mathjax3 from 'markdown-it-mathjax3'
-
 export default {
   title: "Project Docs",
-  description: "Documentation",
-
   base: "/apc/",
-
-  markdown: {
-    config(md) {
-      md.use(mathjax3)
-    },
-  },
-
   themeConfig: {
-    nav: [
-      { text: "MPC", link: "/mpc" },
-      { text: "Process Monitoring", link: "/process-monitoring" },
-    ],
-
     sidebar: [
       {
         text: "Docs",
         items: [
           { text: "Intro", link: "/intro" },
           { text: "MPC", link: "/mpc" },
-          { text: "Process Monitoring", link: "/process-monitoring" },
-        ],
-      },
-    ],
-  },
+          { text: "Process Monitoring", link: "/process-monitoring" }
+        ]
+      }
+    ]
+  }
 }
